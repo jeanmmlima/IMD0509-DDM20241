b5268b83355c713dc9f4b82dbcaccb99442492e6
jeanmmlima <jeanmariomlima@gmail.com>
2024-03-21 18:29:35 -0300

    primeiro commit lugares

---
+++ f03_lugares/lib/components/country_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../models/country.dart';
+
+class CountryItem extends StatelessWidget {
+  final Country country;
+
+  const CountryItem(this.country);
+
+  void _selectedCountry(BuildContext context) {
+    /*
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CountryPlacesScreen(country);
+    }));
+    */
+    Navigator.of(context).pushNamed(
+      '/country-places',
+      arguments: country,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectedCountry(context),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          country.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+                colors: [
+                  country.color.withOpacity(0.5), //cor passad com opacidade
+                  country.color, //cor passada
+                ],
+                begin: Alignment.topLeft, //inicio do gradiente
+                end: Alignment.bottomRight // fim
+                )),
+      ),
+    );
+  }
+}
---
+++ f03_lugares/lib/screens/countries_places_screen.dart
@@ -0,0 +1,25 @@
+import 'package:f03_lugares/models/country.dart';
+import 'package:flutter/material.dart';
+
+class CountryPlacesScreen extends StatelessWidget {
+  //Country country;
+  // CountryPlacesScreen(this.country);
+
+  @override
+  Widget build(BuildContext context) {
+    final Country country =
+        ModalRoute.of(context)!.settings.arguments as Country;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lugares para Conhecer'),
+        //teste
+        //teste
+        //teste
+      ),
+      body: Center(
+        child: Text('País selecionado: ${country.title}', style: TextStyle(fontSize: 20),),
+      ),
+    );
+  }
+}
